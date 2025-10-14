2f5e0670a725df1820ca91cd25de6f029cd9c03e
Finn <finn.tucker@hotmail.com>
2025-10-15 09:54:08 +1100

    - create shader class, stored in separate includes folder - separate shader code from main.cpp in shader.fs and shader.vs - use the magic of the - operator to upside-downify my triangle

--- OpenGL-Learn/main.cpp
+++ OpenGL-Learn/main.cpp
@@ -1,27 +1,7 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
-
-// source code for fragment and vertex shaders (stored here for simplicity)
-const char* vertexShaderSource = "#version 460 core\n"
-"layout (location = 0) in vec3 aPos;\n"
-"layout (location = 1) in vec3 aColour;\n"
-
-"out vec3 ourColour;" // specify a colour output to the fragment shader
-"void main()\n"
-"{\n"
-"   gl_Position = vec4(aPos, 1.0);\n"
-"	ourColour = aColour;\n"
-"}\0";
-
-const char* fragmentShaderSource = "#version 460 core\n"
-"out vec4 FragColor;\n"
-"in vec3 ourColour;\n"
-"void main()\n"
-"{\n"
-"   FragColor = vec4(ourColour, 1.0);\n"
-"}\n\0";
-
+#include <shader.h>
 
 // function forward declarations
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
@@ -53,53 +33,7 @@ int main()
 		return -1;
 	}
 
-	// build and compile shader program
-	// initialise a vertex shader
-	// vertex shader is compiled at runtime from source code
-	// in this basic case, we store the source code as a char* above main. As such, we pass NULL into the source code argument
-	unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
-	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-	glCompileShader(vertexShader);
-
-	// verify that the vertex shader was created successfully. 
-	int success;
-	char infoLog[512];
-	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-
-	if (!success)
-	{
-		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-		std::cout << "ERROR::SHADER::VERTEX::COMPILATION::FAILED" << infoLog << std::endl;
-	}
-
-	unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-	glCompileShader(fragmentShader);
-
-	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
-	if (!success)
-	{
-		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-		std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION::FAILED" << infoLog << std::endl;
-	}
-
-	unsigned int shaderProgram = glCreateProgram();
-	glAttachShader(shaderProgram, vertexShader);
-	glAttachShader(shaderProgram, fragmentShader);
-	glLinkProgram(shaderProgram);
-
-	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-		if (!success)
-		{
-			glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-			std::cout << "ERROR::SHADER::PROGRAM::COMPILATION::FAILED" << infoLog << std::endl;
-		}
-
-	glUseProgram(shaderProgram);
-	glDeleteShader(vertexShader);
-	glDeleteShader(fragmentShader);
-
-
+	Shader shaderProgram("shader.vs", "shader.fs");
 
 	unsigned int VAO1;
 	glGenVertexArrays(1, &VAO1);
@@ -131,37 +65,35 @@ int main()
 	glVertexAttribPointer(1, 3, GL_FLOAT, false, 6 * sizeof(float), (void*)(3 * sizeof(float)));
 	glEnableVertexAttribArray(1);
 
-
-
-	//unsigned int EBO;
-	//glGenBuffers(1, &EBO);
-
-	//// 3 copy indices into a buffer that OpenGL can use
-	//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	//glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(triangles), triangles, GL_STATIC_DRAW);
-	////glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-
 	glViewport(0, 0, 1280, 800);
 
 	while (!glfwWindowShouldClose(window))
 	{
+		// input
+		// -----
 		processInput(window);
 
+		// render
+		// ------
 		glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		glUseProgram(shaderProgram);
+		// render the triangle
+		// -------------------
+		shaderProgram.use();
 		glBindVertexArray(VAO1);
-		float timeValue = glfwGetTime();
-		float greenValue = sin(timeValue) / 2.0f + 0.5f;
-		int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColour");
-		glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
+
+		int vertexColorLocation = glGetUniformLocation(shaderProgram.ID, "ourColour");
 		glDrawArrays(GL_TRIANGLES, 0, 3);
 
-
+		// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+		// -------------------------------------------------------------------------------
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
+	// glfw: terminate, clearing all previously allocated GLFW resources.
+	// ------------------------------------------------------------------
+	glfwTerminate();
 	return 0;
 }
 
---
+++ OpenGL-Learn/shader.fs
@@ -0,0 +1,8 @@
+#version 460 core
+
+out vec4 FragColor;
+in vec3 ourColour;
+void main() 
+{
+    FragColor = vec4(ourColour, 1.0);
+}
